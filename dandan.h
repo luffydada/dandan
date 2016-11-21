a788d7a94021a7be04000911eb055c615cb85c69
luffydada <eddywangying@163.com>
2016-11-21 21:53:09 +0800

    add file

---
+++ dandan.h
@@ -0,0 +1,14 @@
+/***************************************************************************
+*    File name:        dandan.h
+*    Description:
+*    Create date:      2016-11-20 21:37
+*    Modified date:    2016-11-20 21:37
+*    Version:          V1.0
+*    Author:           wangying
+***************************************************************************/
+#ifndef __dandan_h__
+#define __dandan_h__
+
+#include "server/dd_type.h"
+
+#endif //dandan.h
---
+++ server/dd_type.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+*    File name:        dd_type.h
+*    Description:
+*    Create date:      2016-11-20 16:04
+*    Modified date:    2016-11-20 16:05
+*    Version:          V1.0
+*    Author:           wangying
+***************************************************************************/
+#ifndef __dd_type_h__
+#define __dd_type_h__
+
+typedef void ddVoid
+typedef char ddChar
+typedef unsigned char ddUChar
+typedef unsigned char ddByte
+typedef char ddInt8
+typedef unsigned char ddUInt8
+typedef short ddInt16
+typedef unsigned short ddUInt16
+typedef int ddInt
+typedef unsigned int ddUInt
+typedef int ddInt32
+typedef unsigned int ddUInt32
+typedef long long ddInt64
+typedef unsigned long long ddUInt64
+
+typedef ddVoid * ddPointer
+
+#endif //dd_type.h
